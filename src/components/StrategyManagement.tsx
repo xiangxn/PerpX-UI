dd93e6fefc9e3017915a3aa0ae18c15c412f0d56
xiangxn <xiangxn@163.com>
2025-10-16 01:37:06 +0800

    Increase drag velocity threshold for onBack action

--- src/components/StrategyManagement.tsx
+++ src/components/StrategyManagement.tsx
@@ -51,7 +51,7 @@ export default function StrategyManagement({ onBack }: StrategyManagementProps) 
 
   const bind = useGesture({
     onDrag: ({ direction: [xDir], velocity }) => {
-      if (xDir > 0 && velocity > 0.3) {
+      if (xDir > 0 && velocity > 10) {
         onBack();
       }
     },
