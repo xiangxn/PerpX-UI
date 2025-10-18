e95149d6e54dad6b19e6a3bf6127c9580341036d
xiangxn <xiangxn@163.com>
2025-10-18 18:40:20 +0800

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -46,7 +46,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
                     localStorage.removeItem(TOKEN_KEY)
                 }
             } else if (isTMA()) {
-                // initData.restore()
+                initData.restore()
                 console.debug("userInfo:", initData.user())
                 const req = TelegramLoginRequest.create({ initData: initData.raw() })
                 console.log("req:", req)
